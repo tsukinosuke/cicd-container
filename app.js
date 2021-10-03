4d5bdd99d435d447717fcab9dcd084cca9240c45
Admin <admin@moz-vacs.lan>
2021-10-03 13:27:45 -0400

    dd

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello dovlet!"));
+app.get("/", (req, res) => res.send("Hello dovlet1!"));
 
 app.get("/health", (req, res) => {
   res.status(200);
